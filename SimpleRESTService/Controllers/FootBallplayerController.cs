44de17856771f2cce7c1aa3cf7fdec05c847368a
chald80 <72771464+chald80@users.noreply.github.com>
2021-10-01 11:47:48 +0200

    update

---
+++ SimpleRESTService/Controllers/FootBallplayerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Opgave1;
+using SimpleRESTService.Managers;
+
+namespace SimpleRESTService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class FootBallplayerController : Controller
+    {
+        private readonly FootBallPlayerManager _manager = new FootBallPlayerManager();
+
+        
+        [HttpGet]
+        public IEnumerable<FootBallPlayer> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        
+        [HttpGet("{id}")]
+        public FootBallPlayer Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        
+        [HttpPost]
+        public FootBallPlayer Post([FromBody] FootBallPlayer value)
+        {
+            return _manager.Add(value);
+        }
+
+        
+        [HttpPut("{id}")]
+        public FootBallPlayer Put(int id, [FromBody] FootBallPlayer value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        
+        [HttpDelete("{id}")]
+        public FootBallPlayer Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ SimpleRESTService/Managers/FootBallPlayerManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Opgave1;
+
+namespace SimpleRESTService.Managers
+{
+    public class FootBallPlayerManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<FootBallPlayer> Data = new List<FootBallPlayer>
+        {
+            new FootBallPlayer {Id = _nextId++, Name = "Bobo", ShirtNumber = 42, Price = 1000000},
+            new FootBallPlayer {Id=_nextId++, Name = "Lille Lars", ShirtNumber = 89, Price = 6888}
+        };
+
+        public List<FootBallPlayer> GetAll()
+        {
+            return new List<FootBallPlayer>(Data);
+        }
+
+        public FootBallPlayer GetById(int id)
+        {
+            return Data.Find(p => p.Id == id);
+        }
+
+        public FootBallPlayer Add(FootBallPlayer newFootBallPlayer)
+        {
+            newFootBallPlayer.Id = _nextId++;
+            Data.Add(newFootBallPlayer);
+            return newFootBallPlayer;
+        }
+
+        public FootBallPlayer Delete(int id)
+        {
+            FootBallPlayer footBallPlayer = Data.Find(p1 => p1.Id == id);
+            if (footBallPlayer == null) return null;
+            Data.Remove(footBallPlayer);
+            return footBallPlayer;
+        }
+
+        public FootBallPlayer Update(int id, FootBallPlayer updates)
+        {
+            FootBallPlayer footBallPlayer = Data.Find(p1 => p1.Id == id);
+            if (footBallPlayer == null) return null;
+            footBallPlayer.Name = updates.Name;
+            footBallPlayer.ShirtNumber = updates.ShirtNumber;
+            footBallPlayer.Price = updates.Price;
+            return footBallPlayer;
+        }
+    }
+}
